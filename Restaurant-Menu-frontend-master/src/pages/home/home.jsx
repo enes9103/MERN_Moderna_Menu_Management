4490bd116361db1dd76fd8fee2b69f8fa5cded77
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2024-01-13 23:19:34 +0300

    feat: updated home style

--- Restaurant-Menu-frontend-master/src/pages/home/home.jsx
+++ Restaurant-Menu-frontend-master/src/pages/home/home.jsx
@@ -3,9 +3,18 @@ import Logo from "../../asset/logo-without-bg.png";
 
 function Home() {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <h1>Hello Friends</h1>
-      <h2>Welcome to Moderna Menu Management and QRCode Generator Application</h2>
+      <h2>
+        Welcome to Moderna Menu Management Panel and QRCode Generator Application
+      </h2>
       <img src={Logo} alt="Logo" width={400} />
     </div>
   );
