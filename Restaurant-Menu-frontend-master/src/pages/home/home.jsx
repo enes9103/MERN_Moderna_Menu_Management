c3462640331e81e5608484de4e9397d7d2bed51c
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2024-01-14 23:23:24 +0300

    feat: added purchasing plan component in home page

--- Restaurant-Menu-frontend-master/src/pages/home/home.jsx
+++ Restaurant-Menu-frontend-master/src/pages/home/home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "../../asset/logo-without-bg.png";
+import PurchasingPlans from "../../components/PurchasingPlans";
 
 function Home() {
   return (
@@ -13,9 +14,12 @@ function Home() {
     >
       <h1>Hello Friends</h1>
       <h2>
-        Welcome to Moderna Menu Management Panel and QRCode Generator Application
+        Welcome to Moderna Menu Management Panel and QRCode Generator
+        Application
       </h2>
       <img src={Logo} alt="Logo" width={400} />
+
+      <PurchasingPlans />
     </div>
   );
 }
