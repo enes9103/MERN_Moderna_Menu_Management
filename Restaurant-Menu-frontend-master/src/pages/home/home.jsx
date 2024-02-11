80bf5baed35c4dd4dfbb726e501b312bf860fe1c
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2024-02-11 11:58:09 +0300

    Update home.jsx

--- Restaurant-Menu-frontend-master/src/pages/home/home.jsx
+++ Restaurant-Menu-frontend-master/src/pages/home/home.jsx
@@ -5,33 +5,6 @@ import FAQAccordion from "../../components/FAQAccordion";
 import FAQ from "../../components/FAQ";
 
 function Home() {
-  const questions = [
-    {
-      id: 1,
-      question: "Popular Articles",
-      answer:
-        "Suspendisse ipsum elit, hendrerit id eleifend at, condimentum et mauris. Curabitur et libero vel arcu dignissim pulvinar ut ac leo. In sit amet orci et erat accumsan interdum.",
-    },
-    {
-      id: 2,
-      question: "Fix problems & request removals",
-      answer:
-        "Suspendisse ipsum elit, hendrerit id eleifend at, condimentum et mauris. Curabitur et libero vel arcu dignissim pulvinar ut ac leo. In sit amet orci et erat accumsan interdum.",
-    },
-    {
-      id: 3,
-      question: "Browse the web",
-      answer:
-        "Suspendisse ipsum elit, hendrerit id eleifend at, condimentum et mauris. Curabitur et libero vel arcu dignissim pulvinar ut ac leo. In sit amet orci et erat accumsan interdum.",
-    },
-    {
-      id: 4,
-      question: "Search on your phone or tablet",
-      answer:
-        "Suspendisse ipsum elit, hendrerit id eleifend at, condimentum et mauris. Curabitur et libero vel arcu dignissim pulvinar ut ac leo. In sit amet orci et erat accumsan interdum.",
-    },
-  ];
-
   return (
     <div
       style={{
@@ -52,7 +25,6 @@ function Home() {
 
       <FAQAccordion />
 
-      <FAQ data={questions} />
     </div>
   );
 }
