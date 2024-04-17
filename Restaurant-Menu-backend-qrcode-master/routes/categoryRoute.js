24a542f59d4258558bf3d4a1aa3b818efb5ac57e
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2024-04-17 23:10:35 +0300

    Update categoryRoute.js

--- Restaurant-Menu-backend-qrcode-master/routes/categoryRoute.js
+++ Restaurant-Menu-backend-qrcode-master/routes/categoryRoute.js
@@ -1,19 +1,13 @@
 import express from "express";
 import { adminAuth } from "../config/auth.js";
 
-
 const router = express.Router();
-
 import {
     getAllCategories,
     createCategory,
-  
-    
 } from "../controllers/categoryController.js";
-
 
 router.get('/', getAllCategories);
 router.post('/', createCategory);
-
 
 export default router
