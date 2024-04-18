f5e46c62535a224ba63b375c80405f1e27e8d2b6
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2024-04-18 19:24:37 +0300

    Update productRoute.js

--- Restaurant-Menu-backend-qrcode-master/routes/productRoute.js
+++ Restaurant-Menu-backend-qrcode-master/routes/productRoute.js
@@ -1,21 +1,16 @@
 import express from "express";
 import { adminAuth } from "../config/auth.js";
 
-
 const router = express.Router();
 
 import {
     getAllProduct,
     createProduct,
     updatePrice,
-    
 } from "../controllers/productController.js";
-
 
 router.get('/', getAllProduct);
 router.post('/', createProduct);
 router.put('/:id', updatePrice);
-
-
 
 export default router
