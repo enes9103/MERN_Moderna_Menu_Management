15b3a823df54d36dbd5e2238323081ac55dbcb96
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2024-04-27 13:26:05 +0300

    Update db.js

--- Restaurant-Menu-backend-qrcode-master/config/db.js
+++ Restaurant-Menu-backend-qrcode-master/config/db.js
@@ -6,12 +6,10 @@ try {
   const conn = await mongoose.connect(process.env.MONGO_URL, {
    useUnifiedTopology: true,
 });
-
-   console.log(`MongoDB Connected: ${conn.connection.host}`);
 } catch (error) {
-    console.log(`Error: ${error.message}`);
-process.exit(1);
-}
+  console.log(`Error: ${error.message}`);
+  process.exit(1);
+  }
 };
 
 export default connectDB;
