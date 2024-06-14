2a56791c8473e94a6839d8d54bc05f6fe5f21cee
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2024-06-14 21:16:38 +0300

    Update Chart.jsx

--- Restaurant-Menu-frontend-master/src/components/Chart.jsx
+++ Restaurant-Menu-frontend-master/src/components/Chart.jsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { useTheme } from "@mui/material/styles";
 import { LineChart, axisClasses } from "@mui/x-charts";
-
 import Title from "./Title";
 
 // Generate Sales Data
